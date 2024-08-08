4e2e850e6ac01103a3dc192fadc402ddfb526b4f
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-08-08 17:40:17 +0000

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """
-Route module for the API.
-Handles all routes and applies authentication mechanisms.
+Route module for the API
 """
-
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 
+# Dynamically load the appropriate authentication class
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -25,26 +25,30 @@ elif AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 @app.before_request
-def before_request():
+def before_request_handler():
     """
-    Filter each request before it's handled by the appropriate route.
-    This function ensures that the user is authenticated before accessing
-    certain endpoints.
+    Filter each request before it's handled by the proper route
     """
     if auth is None:
         return
 
     request.current_user = auth.current_user(request)
-    excluded_paths = [
+    excluded = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
         '/api/v1/auth_session/login/'
     ]
 
-    if auth.require_auth(request.path, excluded_paths):
+    if auth.require_auth(request.path, excluded):
         cookie = auth.session_cookie(request)
         if auth.authorization_header(request) is None and cookie is None:
             abort(401, description="Unauthorized")
@@ -53,21 +57,23 @@ def before_request():
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Handler for 404 Not Found errors."""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Handler for 401 Unauthorized errors."""
+    """ Request unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Handler for 403 Forbidden errors."""
+    """ Request forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
