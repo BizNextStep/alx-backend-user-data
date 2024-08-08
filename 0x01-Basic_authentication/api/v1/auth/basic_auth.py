c28c4a12d6a8649e43f97e0d0b6bbf1b60f4badd
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-08-08 11:30:37 +0000

    basic_auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for handling Basic Authentication.
+"""
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth for managing Basic Authentication.
+    """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 encoded part of the Authorization header if valid, otherwise None.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[len("Basic "):]
+
