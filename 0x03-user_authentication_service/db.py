3cf57f4e71dfca672eba7db1e2f84e69e32e10ca
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-08-15 14:51:34 +0000

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """DB module
+This module provides the DB class for managing database interactions using SQLAlchemy.
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
 
 
 class DB:
     """DB class
+    This class handles the database connection and user management.
     """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
+        This constructor sets up the database engine and creates the necessary tables.
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
@@ -24,6 +26,7 @@ class DB:
     @property
     def _session(self) -> Session:
         """Memoized session object
+        This property creates and returns a new session if one does not already exist.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
